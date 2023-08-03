d48f343775d32a7a74d402319e93a7cc8b493dec
woynert <whayner.porras.2020@upb.edu.co>
2023-08-02 20:56:23 -0500

    feat(url-to-pdf): Method to execute cli command

--- threaded-url-to-pdf/main.cpp
+++ threaded-url-to-pdf/main.cpp
@@ -1,24 +1,41 @@
 #include <iostream>
+#include <stdexcept>
 #include <string>
 #include <vector>
 
 #include "absl/flags/flag.h"
+#include "absl/flags/internal/flag.h"
 #include "absl/flags/parse.h"
 #include "absl/strings/str_split.h"
 
-ABSL_FLAG(std::string, urls, "", "Comma separated list of URLs to convert");
+ABSL_FLAG (std::string, urls, "", "Comma separated list of URLs to convert");
 
-int main (int argc, char *argv[])
+void execute (std::string cmd)
 {
-    absl::ParseCommandLine(argc, argv);
+	FILE* pipe = popen (cmd.c_str (), "r");
 
-    // get urls 
+	if (WEXITSTATUS (pclose (pipe)) != 0) {
+		throw std::runtime_error ("Failed to open chromium");
+	}
+}
 
-    std::vector<std::string> urls = absl::StrSplit(absl::GetFlag(FLAGS_urls), ",");
-    for (auto url: urls)
-        std::cout << url << std::endl;
+int main (int argc, char* argv[])
+{
+	absl::ParseCommandLine (argc, argv);
 
+	// get urls
 
-    std::cout << "Hello " << absl::GetFlag(FLAGS_count) << std::endl;
-    return 0;
+	if (absl::GetFlag(FLAGS_urls) == "")
+	{
+		std::cout << "No urls provided\n";
+		return 1;
+	}
+
+	std::vector<std::string> urls =
+		absl::StrSplit (absl::GetFlag (FLAGS_urls), ",");
+
+	for (auto url : urls)
+		std::cout << url << std::endl;
+
+	return 0;
 }
