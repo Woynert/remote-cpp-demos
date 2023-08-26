445ea48e11d6bd3b2351d472c9330f520141d3dd
woynert <whayner.porras.2020@upb.edu.co>
2023-08-25 20:29:27 -0500

    feat: Simple SOAP client with info printing

--- simple-soap/client/src/main/java/simple/client/App.java
+++ simple-soap/client/src/main/java/simple/client/App.java
@@ -1,18 +1,38 @@
 package simple.client;
 
+import java.net.URL;
 import javax.xml.ws.Endpoint;
+import simple.generated.soap.SampleService;
+import simple.generated.soap.SampleServiceImpService;
 import simple.model.*;
+import simple.soapcast.*;
 
 public class App
 {
 	public static void main (String[] args)
 	{
+		System.out.println ("Client: Consuming SOAP");
 
-		System.out.println ("Gateway: Starting SOAP");
+		// consume SOAP
 
-		Song song = new Song ("A", "B", 1, 1);
+		try {
 
-		// serve SOAP
-		// Endpoint.publish ("http://0.0.0.0:8080/sampleservice", new SampleServiceImp ());
+			URL url = new URL ("http://localhost:8080/sampleservice?wsdl");
+
+			SampleServiceImpService sampleService = new SampleServiceImpService (url);
+			SampleService service = sampleService.getSampleServiceImpPort ();
+
+			// print amount of songs
+
+			System.out.println (String.format ("Amount of songs: %d", service.getSongAmount ()));
+
+			// print song info
+
+			Storage storage = CastStorage.cast (service.getSongStorage ());
+			storage.print ();
+
+		} catch (Exception e) {
+			System.err.println (e);
+		}
 	}
 }
